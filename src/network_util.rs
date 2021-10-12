9188c2d6ce13b8b9d50145a2bec5c9c8eaacbc5d
Ivan Schuetz <ivanschuetz@gmail.com>
2021-10-12 14:32:14 +0200

    Increase pending tx timeout

--- src/network_util.rs
+++ src/network_util.rs
@@ -7,14 +7,16 @@ pub async fn wait_for_pending_transaction(
     algod: &Algod,
     tx_id: &str,
 ) -> Result<Option<PendingTransaction>, AlgonautError> {
-    let timeout = Duration::from_secs(10);
-    let start = Instant::now();
+    let timeout = Duration::from_secs(60);
+    let start = Instant::now();           
+     log::debug!("Start waiting for pending tx confirmation..");
     loop {
         let pending_transaction = algod.pending_transaction_with_id(tx_id).await?;
         // If the transaction has been confirmed or we time out, exit.
         if pending_transaction.confirmed_round.is_some() {
             return Ok(Some(pending_transaction));
         } else if start.elapsed() >= timeout {
+            log::debug!("Timeout waiting for pending tx");
             return Ok(None);
         }
         sleep(250).await;
