1bd8703f5e4f034f57db00407b93e03618c49ab2
Ivan Schuetz <ivanschuetz@gmail.com>
2021-12-13 19:53:40 +0100

    Support variable sandbox accounts

--- scripts/sandbox_reset_for_tests.sh
+++ scripts/sandbox_reset_for_tests.sh
@@ -6,15 +6,25 @@ sandbox up dev
 # reset sandbox
 sandbox reset
 
+# get a funder account (the sandbox accounts are variable - change when the sandbox environment is recreated)
+ACCOUNTS_OUTPUT=$(sandbox goal account list)
+echo $ACCOUNTS_OUTPUT
+for acct in $(echo "$ACCOUNTS_OUTPUT" | cut -f 3 |tr -s ' '); do
+    ACCOUNTS+=($acct)
+done
+FUNDER=${ACCOUNTS[0]}
+echo "Funding account:"
+echo $FUNDER
+
 # import additional account
 # MKRBTLNZRS3UZZDS5OWPLP7YPHUDNKXFUFN5PNCJ3P2XRG74HNOGY6XOYQ
 sandbox goal account import -m "clog coral speak since defy siege video lamp polar chronic treat smooth puzzle input payment hobby draft habit race birth ridge correct behave able close"
 
-# fund additional account
-
-FUNDER=VKCFMGBTVINZ4EN7253QVTALGYQRVMOLVHF6O44O2X7URQP7BAOAXXPFCA
-# echo $FUNDER
+# fund our test accounts
 # 10_000 algos
+sandbox goal clerk send -a 10000000000 -f $FUNDER -t VKCFMGBTVINZ4EN7253QVTALGYQRVMOLVHF6O44O2X7URQP7BAOAXXPFCA
+sandbox goal clerk send -a 10000000000 -f $FUNDER -t WZOKN67NQUMY5ZV7Q2KOBKUY5YP3L5UFFOWBUV6HKXKFMLCUWTNZJRSI4E
+sandbox goal clerk send -a 10000000000 -f $FUNDER -t ZRPA4PEHLXIT4WWEKXFJMWF4FNBCA4P4AYC36H7VGNSINOJXWSQZB2XCP4
 sandbox goal clerk send -a 10000000000 -f $FUNDER -t MKRBTLNZRS3UZZDS5OWPLP7YPHUDNKXFUFN5PNCJ3P2XRG74HNOGY6XOYQ
 
 # temporary: fund customer payment amount
--- src/testing/test_data.rs
+++ src/testing/test_data.rs
@@ -7,21 +7,21 @@ use algonaut::transaction::account::Account;
 
 #[cfg(test)]
 pub fn creator() -> Account {
-    // sandbox
+    // VKCFMGBTVINZ4EN7253QVTALGYQRVMOLVHF6O44O2X7URQP7BAOAXXPFCA
     Account::from_mnemonic("town clutch grain accident sheriff wagon meadow shaft saddle door all town supply indicate deliver about arrange hire kit curve destroy gloom attitude absorb excite").unwrap()
 }
 
 #[allow(dead_code)]
 #[cfg(test)]
 pub fn investor1() -> Account {
-    // sandbox
+    // WZOKN67NQUMY5ZV7Q2KOBKUY5YP3L5UFFOWBUV6HKXKFMLCUWTNZJRSI4E
     Account::from_mnemonic("phone similar album unusual notable initial evoke party garlic gain west catch bike enforce layer bring suggest shiver script venue couple tooth special abandon ranch").unwrap()
 }
 
 #[allow(dead_code)]
 #[cfg(test)]
 pub fn investor2() -> Account {
-    // sandbox
+    // ZRPA4PEHLXIT4WWEKXFJMWF4FNBCA4P4AYC36H7VGNSINOJXWSQZB2XCP4
     Account::from_mnemonic("abandon include valid approve among begin disorder hint option train palace drink enable enter shallow various bid jacket record left derive memory magnet able phrase").unwrap()
 }
 
