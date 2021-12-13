de1b9f69e0e16dd8edd3ef6ad849283514fe42f1
Ivan Schuetz <ivanschuetz@gmail.com>
2021-12-13 15:30:58 +0100

    Integrate SDKs test environment
    Tried with sandbox - has issues - the SDK's test environment works for Github actions

--- scripts/sandbox_reset_for_tests.sh
+++ scripts/sandbox_reset_for_tests.sh
@@ -1,21 +1,32 @@
 # run before each test
 
 # ensure sandbox is up
-sandbox up dev
+# sandbox up dev
 
 # reset sandbox
-sandbox reset
+# sandbox reset
+./test-harness/scripts/restart.sh
+
+# get a funder account (the sandbox accounts are variable - change when the sandbox environment is recreated)
+ACCOUNTS_OUTPUT=$(goal account list)
+echo $ACCOUNTS_OUTPUT
+for acct in $(echo "$ACCOUNTS_OUTPUT" | cut -f 3 |tr -s ' '); do
+    ACCOUNTS+=($acct)
+done
+FUNDER=${ACCOUNTS[0]}
+echo "Funding account:"
+echo $FUNDER
 
 # import additional account
 # MKRBTLNZRS3UZZDS5OWPLP7YPHUDNKXFUFN5PNCJ3P2XRG74HNOGY6XOYQ
-sandbox goal account import -m "clog coral speak since defy siege video lamp polar chronic treat smooth puzzle input payment hobby draft habit race birth ridge correct behave able close"
+goal account import -m "clog coral speak since defy siege video lamp polar chronic treat smooth puzzle input payment hobby draft habit race birth ridge correct behave able close"
 
-# fund additional account
-
-FUNDER=VKCFMGBTVINZ4EN7253QVTALGYQRVMOLVHF6O44O2X7URQP7BAOAXXPFCA
-# echo $FUNDER
+# fund our test accounts
 # 10_000 algos
-sandbox goal clerk send -a 10000000000 -f $FUNDER -t MKRBTLNZRS3UZZDS5OWPLP7YPHUDNKXFUFN5PNCJ3P2XRG74HNOGY6XOYQ
+goal clerk send -a 10000000000 -f $FUNDER -t VKCFMGBTVINZ4EN7253QVTALGYQRVMOLVHF6O44O2X7URQP7BAOAXXPFCA
+goal clerk send -a 10000000000 -f $FUNDER -t WZOKN67NQUMY5ZV7Q2KOBKUY5YP3L5UFFOWBUV6HKXKFMLCUWTNZJRSI4E
+goal clerk send -a 10000000000 -f $FUNDER -t ZRPA4PEHLXIT4WWEKXFJMWF4FNBCA4P4AYC36H7VGNSINOJXWSQZB2XCP4
+goal clerk send -a 10000000000 -f $FUNDER -t MKRBTLNZRS3UZZDS5OWPLP7YPHUDNKXFUFN5PNCJ3P2XRG74HNOGY6XOYQ
 
 # temporary: fund customer payment amount
 # to ease manual testing, to not have to send a customer payment first
@@ -23,4 +34,4 @@ sandbox goal clerk send -a 10000000000 -f $FUNDER -t MKRBTLNZRS3UZZDS5OWPLP7YPHU
 # sandbox goal clerk send -a 10000000000 -f $FUNDER -t 3BW2V2NE7AIFGSARHF7ULZFWJPCOYOJTP3NL6ZQ3TWMSK673HTWTPPKEBA -d net1/Node
 
 echo "done!"
-sandbox goal account list
+goal account list
--- src/dependencies.rs
+++ src/dependencies.rs
@@ -8,7 +8,7 @@ pub fn algod() -> Algod {
 #[allow(dead_code)]
 fn private_network_algod() -> Algod {
     AlgodBuilder::new()
-        .bind("http://127.0.0.1:4001")
+        .bind("http://127.0.0.1:60000")
         .auth("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         .build_v2()
         .expect("Couldn't initialize algod")
