5c431b625e3f784692c4ad6cef53474904aeacfc
Ivan Schuetz <ivanschuetz@gmail.com>
2022-03-14 14:00:30 +0100

    Supress not used warning
    It likely makes sense to limit this to tests config. For now just suppressing the warning.

--- src/files.rs
+++ src/files.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+#[allow(dead_code)]
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
