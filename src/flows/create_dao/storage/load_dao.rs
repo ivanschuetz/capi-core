56a64472cb400ba7b2099111e07f3c805e1c515f
Ivan Schuetz <ivanschuetz@gmail.com>
2022-03-22 20:53:08 +0100

    Implement to string / from string for dao id

--- src/flows/create_dao/storage/load_dao.rs
+++ src/flows/create_dao/storage/load_dao.rs
@@ -133,6 +133,20 @@ impl DaoId {
         self.0.bytes()
     }
 }
+
+impl FromStr for DaoId {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let app_id: DaoAppId = s.parse()?;
+        Ok(DaoId(app_id))
+    }
+}
+impl ToString for DaoId {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
+
 impl TryFrom<&[u8]> for DaoId {
     type Error = anyhow::Error;
     fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
