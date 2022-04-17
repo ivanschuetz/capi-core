0763c5de82ec84af304127be2d02904de6a619bd
Ivan Schuetz <ivanschuetz@gmail.com>
2022-04-17 11:30:40 +0200

    Add commented commands to remove the images
    might uncomment later - for now only used if the other commands don't help

--- scripts/nuke_and_restart_sandbox.sh
+++ scripts/nuke_and_restart_sandbox.sh
@@ -5,5 +5,9 @@ docker system prune -a
 docker rm -f (docker ps -a -q)
 docker volume rm (docker volume ls -qf dangling=true)
 
+# docker rmi sandbox_algod
+# docker rmi sandbox_indexer
+# docker rmi postgres
+
 # start (dev mode)
 sandbox up dev -v
