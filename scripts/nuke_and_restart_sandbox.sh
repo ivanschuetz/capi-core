a3aea9126525da357fe620e09093f645f83e4d11
Ivan Schuetz <ivanschuetz@gmail.com>
2022-04-20 09:28:52 +0200

    Improve nuke sandbox script
    some lines fail at least on zsh / fish, unclear use - for now commented, uncommented lines removing images

--- scripts/nuke_and_restart_sandbox.sh
+++ scripts/nuke_and_restart_sandbox.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
 # sometimes the indexer gets stuck
 # so far only completely resetting the environment fixes it
+
 docker system prune -a -f --volumes
 docker system prune -a
-docker rm -f (docker ps -a -q)
-docker volume rm (docker volume ls -qf dangling=true)
 
-# docker rmi sandbox_algod
-# docker rmi sandbox_indexer
-# docker rmi postgres
+# syntax errors in zsh / fish
+# docker rm -f (docker ps -a -q)
+# docker volume rm (docker volume ls -qf dangling=true)
+
+docker rmi sandbox_algod
+docker rmi sandbox_indexer
+docker rmi postgres
 
 # start (dev mode)
 sandbox up dev -v
